07e9f6791c14e7f6c8a1e70e2ebb28c37e8e3721
Bobby Joe Smith III <bsmith07@risd.edu>
2020-04-10 22:07:09 -0400

    Add comment to draggable demo

--- Draggable/script.js
+++ Draggable/script.js
@@ -1,3 +1,3 @@
 // Use JQuery to target an HTML element and apply draggable functionality to it
-
+// I am selecting the html element by the class I assigned them "draggable_element". You can use any css selector to target an HTML element with JQuery, as long as it's in quotation marks.
 $(".draggable_element").draggable();
